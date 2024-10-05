4f03d70b16af8c59cadac4c6c11f1cd046ce369a
Quoc Nam <Buiquocnam1012@gmail.com>
2024-10-06 01:34:47 +0700

    create HEADER, NAVIGATION

--- app/src/main/java/com/example/appmoviednk/activity/MainActivity.java
+++ app/src/main/java/com/example/appmoviednk/activity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.appmoviednk.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,11 +18,16 @@ import com.example.appmoviednk.fragment.HomeFragment;
 import com.example.appmoviednk.R;
 import com.example.appmoviednk.fragment.VoucherFragment;
 import com.example.appmoviednk.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
     //không cần findViewById()
     ActivityMainBinding binding;
+    ImageView btnMenu;
+    ImageView exitNav;
+    NavigationView nvgt;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,61 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
+        ImageView imgAccount = findViewById(R.id.img_account);
+        ImageView imgLogo = findViewById(R.id.img_logo);
+        ImageView imgMenu = findViewById(R.id.img_menu);
+
+        // Thiết lập sự kiện click cho imgAccount
+        imgAccount.setOnClickListener(v -> {
+
+        });
+
+        // Thiết lập sự kiện click cho imgLogo
+        imgLogo.setOnClickListener(v -> {
+            replaceFragment(new HomeFragment());
+            binding.bottomNavigationView.setSelectedItemId(R.id.home);
+        });
+
+        // Thiết lập sự kiện click cho imgMenu
+        imgMenu.setOnClickListener(v -> {
+
+        });
+
+        btnMenu = findViewById(R.id.img_menu);
+        nvgt = findViewById(R.id.navigation);
+        View headerView = binding.navigation.getHeaderView(0);
+        exitNav = headerView.findViewById(R.id.cancelButton);
+        drawerLayout = findViewById(R.id.main);
+        if (exitNav == null) {
+            Log.e("MainActivity", "exitNav is null");
+        }
+        btnMenu.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerOpen(nvgt)) {
+                    drawerLayout.closeDrawer(nvgt);
+                } else {
+                    drawerLayout.openDrawer(nvgt);
+                }
+            }
+        });
+        exitNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Đóng navigation drawer nếu nó đang mở
+
+                    drawerLayout.closeDrawer(findViewById(R.id.navigation));
+            }
+        });
+        binding.navigation.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home){
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.nav_calcu){
+                replaceFragment(new VoucherFragment());
+            }
+            drawerLayout.closeDrawer(findViewById(R.id.navigation));
+            return true;
+        });
     }
 
     private void replaceFragment(Fragment fragment) {
